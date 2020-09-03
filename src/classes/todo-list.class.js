06c1f9e7b76a8ed68f1fb9bc78bc63a1f2555429
José Ponce <jponce2492@gmail.com>
2020-09-03 13:36:09 -0400

    Guardar y recuperar todos

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -3,17 +3,20 @@
 export class TodoList{
     constructor(){
         
-        this.todos=[];
+        // this.todos=[];
+        this.cargarLocalStorage();
 
     }
 
 
     nuevoTodo( todo ){
         this.todos.push( todo );
+        this.guardarLocalStorage();
     }
 
     eliminarTodo( id ){
         this.todos = this.todos.filter(todo => todo.id != id);
+        this.guardarLocalStorage();
     }
 
     marcarCompletado( id ){
@@ -22,6 +25,7 @@ export class TodoList{
             console.log(id, todo.id);
             if (todo.id == id){
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -29,5 +33,15 @@ export class TodoList{
 
     eliminarCompletados(){
         this.todos = this.todos.filter(todo => !todo.completado);
+        this.guardarLocalStorage();
+    }
+    guardarLocalStorage(){
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+    cargarLocalStorage(){
+       
+        this.todos = ( localStorage.getItem('todo') )
+                        ? JSON.parse( localStorage.getItem('todo') )
+                        : [];
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,5 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JS!!');
 
-todoList.nuevoTodo( tarea );
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
+todoList.todos.forEach( crearTodoHtml );
