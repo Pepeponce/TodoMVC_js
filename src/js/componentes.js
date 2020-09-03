4ef934d2e7892baf96d21d43c93837e2d61e6fbd
José Ponce <jponce2492@gmail.com>
2020-09-03 14:39:41 -0400

    aplicar filtos

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,13 +1,12 @@
-//referencias
 import { Todo } from '../classes';
 import  { todoList } from '../index';
 
-
-const divTodoList = document.querySelector('.todo-list');
-const txtInput = document.querySelector('.new-todo');
-const btnBorrar = document.querySelector('.clear-completed');
-
-
+//referencias en el html
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
+const ulFiltros     = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
 export const crearTodoHtml = ( todo ) => {
 
@@ -72,8 +71,43 @@ btnBorrar.addEventListener('click', ()=>{
         const elemento = divTodoList.children[i];
 
         if(elemento.classList.contains('completed')){
-
             divTodoList.removeChild( elemento );
         }
     }
+});
+
+ulFiltros.addEventListener('click', (event) => {
+
+    const filtro = event.target.text;
+    if( !filtro ){ return; }
+
+    anchorFiltros.forEach( elem => elem.classList.remove('selected') );
+    event.target.classList.add('selected');
+
+    for( const elemento of divTodoList.children ) {
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch( filtro ) {
+
+            case 'Pendientes':
+                if( completado ) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+            case 'Completados':
+                if( !completado ) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+        }
+
+
+    }
+
+
+
 });
